39f2d721e586f8fb3579a20856117406a50dd3b9
Marko Vuksanovic <markovuksanovic@gmail.com>
2017-02-13 23:55:17 +1100

    :sunrise:

---
+++ README.md
@@ -0,0 +1,5 @@
+### acat
+
+A simple utility to transform json documents into json documents usable by `aws dynamodb` commands.
+
+
---
+++ index.js
@@ -0,0 +1,48 @@
+#!/usr/bin/env node
+
+var program = require('commander');
+var marshalItem = require('dynamodb-marshaler').marshalItem;
+var fs = require('fs');
+
+program
+    .version('1.0.0')
+    .option('-p, --prefix [prefix]', 'Prefix generated files with this name [transformed]', 'transformed')
+    .option('-d, --dest [build]', 'Folder in which output will be placed [build]', 'build')
+    .usage('[options] <file>')
+    .parse(process.argv);
+if(!program.args.length) {
+    program.help();
+}
+
+var fileContent= fs.readFileSync(program.args[0], 'utf8')
+var items = JSON.parse(fileContent);
+
+function maybeCreateDir(dirName, next) {
+  fs.stat(dirName, (err, stats) => {
+    if (err) {
+      if (err.code === 'ENOENT')
+      {
+        fs.mkdirSync(dirName);
+      }
+      else {
+        console.error(err);
+        process.exit(err.code);
+      }
+      next();
+    }
+  })
+}
+
+function processItems(items) {
+	for (var i = 0; i < items.length; i++) {
+    fs.writeFile(`${program.dest}/${program.prefix}${i}.json`,     JSON.stringify(marshalItem(items[i])), (err) => {
+        if (err) {
+          console.error(err);
+          process.exit(err.code);
+        }
+      })
+	  };
+}
+
+maybeCreateDir(program.dest, () => { processItems(items) });
+
