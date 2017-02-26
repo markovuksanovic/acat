55b63bf989719abcc77378c20c10d94dbe72c592
Marko Vuksanovic <markovuksanovic@gmail.com>
2017-02-26 19:13:14 +1100

    Specify -m (default) or -u parameter to do marshaling (and unmarshaling, respectively) of JSON <--> AWS DynamoDB JSON.

--- index.js
+++ index.js
@@ -2,16 +2,31 @@
 
 var program = require('commander');
 var marshalItem = require('dynamodb-marshaler').marshalItem;
+var unmarshalItem = require('dynamodb-marshaler').unmarshalItem;
 var fs = require('fs');
 
 program
     .version('1.0.0')
     .option('-p, --prefix [prefix]', 'Prefix generated files with this name [transformed]', 'transformed')
+    .option('-m, --marshal', 'Marshal JSON into AWS dynamodb compatible json. This is default in case -m or -u is not specified.')
+    .option('-u, --unmarshal', 'Unmarshal JSON from AWS dynamodb compatible json. Excludes -m.')
     .option('-d, --dest [build]', 'Folder in which output will be placed [build]', 'build')
     .usage('[options] <file>')
     .parse(process.argv);
 if(process.stdin.isTTY && !program.args.length) {
     program.help();
+}
+
+function shouldMarshal(program) {
+  if (!program.marshal && !program.unmarshal) {
+    return true;
+  } else if (program.marshal && program.unmarshal) {
+    throw "-m and -u cannot be specified at the same time."
+  } else if (program.marshal) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function readFileContent(filePath) {
@@ -55,7 +70,7 @@ function maybeCreateDir(dirName, next) {
   })
 }
 
-function processItems(items) {
+function marshalJson(items) {
 	for (var i = 0; i < items.length; i++) {
     fs.writeFile(`${program.dest}/${program.prefix}${i}.json`, JSON.stringify(marshalItem(items[i])), (err) => {
         if (err) {
@@ -66,6 +81,15 @@ function processItems(items) {
 	  };
 }
 
+function unrshalJson(json) {
+  fs.writeFile(`${program.dest}/${program.prefix}.json`, JSON.stringify(unmarshalItem(json)), (err) => {
+    if (err) {
+      console.error(err);
+      process.exit(err.code);
+    }
+  })
+}
+
 let inputStreamPromise;
 if (program.args[0]) {
   inputStreamPromise = readFileContent(program.args[0])
@@ -73,8 +97,14 @@ if (program.args[0]) {
   inputStreamPromise = readInputStreamAsData();
 }
 
-inputStreamPromise.then((items) => {
-    maybeCreateDir(program.dest, () => { processItems(items) });
+inputStreamPromise.then((json) => {
+    maybeCreateDir(program.dest, () => { 
+      if (shouldMarshal(program)) {
+        marshalJson(json) 
+      } else {
+        unrshalJson(json)
+      }
+    });
   })
   .catch((err) => {
     console.error(err);
